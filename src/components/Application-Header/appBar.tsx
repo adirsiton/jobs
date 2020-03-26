c9ddd89565cf1d90681d4289d8fe1a96dea121fb
Shahar Anati <shahar.anati@gmail.com>
2020-03-26 17:09:27 +0200

    create navbar

---
+++ src/components/Application-Header/appBar.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import { User } from '../../types/userTypes';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+interface AppBarData {
+    user: User;
+}
+
+type AppBarProps = AppBarData;
+
+export const appBar: React.FC<AppBarProps> = (props): JSX.Element => {
+    
+    const { user } = props;
+
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="h5">
+                    {user.userInitials}
+                </Typography>
+                <Typography variant="h3">
+                    Meshushim
+                </Typography>
+            </Toolbar> 
+        </AppBar>
+    )
+}
