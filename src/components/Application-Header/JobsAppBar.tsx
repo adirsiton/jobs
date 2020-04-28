c1048def52d2217dbe7b5eb1e4ba4e7baf826a5b
Shany Guy <gshany@gmail.com>
2020-04-28 12:10:45 +0300

    styling jobs app bar

--- src/components/Application-Header/JobsAppBar.tsx
+++ src/components/Application-Header/JobsAppBar.tsx
@@ -1,69 +1,73 @@
-import * as React from 'react';
+import * as React from "react";
 
-import { inject } from 'mobx-react';
-import { Link } from 'react-router-dom';
+import { inject } from "mobx-react";
+import { Link } from "react-router-dom";
 
-import { withStyles, WithStyles } from '@material-ui/core';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Avatar from '@material-ui/core/Avatar';
-import Tooltip from '@material-ui/core/Tooltip';
+import { withStyles, WithStyles } from "@material-ui/core";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
+import Tooltip from "@material-ui/core/Tooltip";
 
-import  jobsLogo from '../../assets/images/jobsLogo.png';
-import { UserStore } from '../../store/UserStore';
-import FavoriteList from './favorite-list/FavoriteList';
-import styles from './JobsAppBarStyle';
+import jobsLogo from "../../assets/images/jobsLogo.png";
+import { UserStore } from "../../store/UserStore";
+import FavoriteList from "./favorite-list/FavoriteList";
+import styles from "./JobsAppBarStyle";
 
 interface AppBarDataProps extends WithStyles<typeof styles> {
-    userStore?: UserStore;
+  userStore?: UserStore;
 }
 
 const JobsAppBar: React.FC<AppBarDataProps> = (props): JSX.Element => {
-    const { classes } = props;
+  const { classes } = props;
 
-    const userStore: UserStore = props.userStore!;
+  const userStore: UserStore = props.userStore!;
 
-    const getUserDetails = (): JSX.Element => {
-        return (
-            <div className={classes.userDetails}>
-                {/* Todo tooltip dosnt work, fix it */}
-                <Tooltip placement="right" title="ג'ובים ששמרתי" aria-label="my favorites">
-                    <FavoriteList/>
-                </Tooltip>
-                <Link to='/personal'>
-                    <Avatar className={classes.avatar}>
-                        {userStore.getUserInitials}
-                    </Avatar>
-                </Link>
-            </div>
-        );
-    }
-
-    const getLogo = (): JSX.Element => {
-        return (
-            <div className={classes.logoContainer}>
-                <Link to='/'  >
-                    <img src={jobsLogo} alt="jobs_logo"/>
-                </Link>
-                <Typography variant="h3" className={classes.logoSystemName}>
-                    ג'ובניק
-                </Typography>
-                <Typography variant="h6" className={classes.logoSubTitle}>
-                    מוצא לך את הג'וב הבא
-                </Typography>
-            </div>
-        );
-    }
-    
+  const getUserDetails = (): JSX.Element => {
     return (
-        <AppBar className={classes.root} position="static">
-            <Toolbar className={classes.toolbar}>
-                {getLogo()}
-                {getUserDetails()}
-            </Toolbar> 
-        </AppBar>
+      <div className={classes.userDetails}>
+        {/* Todo tooltip dosnt work, fix it */}
+        <Tooltip
+          placement='right'
+          title="ג'ובים ששמרתי"
+          aria-label='my favorites'
+        >
+          <FavoriteList />
+        </Tooltip>
+        <Link to='/personal'>
+          <Avatar className={classes.avatar}>
+            {userStore.getUserInitials}
+          </Avatar>
+        </Link>
+      </div>
     );
-}
+  };
 
-export default inject('userStore')(withStyles(styles)(JobsAppBar));
+  const getLogo = (): JSX.Element => {
+    return (
+      <div className={classes.logoContainer}>
+        <Link to='/'>
+          <img src={jobsLogo} alt='jobs_logo' className={classes.logoIcon} />
+        </Link>
+        <Typography variant='h3' className={classes.logoSystemName}>
+          ג'ובניק
+        </Typography>
+        <Typography variant='h6' className={classes.logoSubTitle}>
+          מוצא לך את הג'וב הבא
+        </Typography>
+      </div>
+    );
+  };
+
+  return (
+    <AppBar className={classes.root} position='static'>
+      <Toolbar className={classes.toolbar}>
+        {getLogo()}
+        {getUserDetails()}
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default inject("userStore")(withStyles(styles)(JobsAppBar));
--- src/components/Application-Header/JobsAppBarStyle.ts
+++ src/components/Application-Header/JobsAppBarStyle.ts
@@ -1,46 +1,50 @@
-import { createStyles } from '@material-ui/core';
-import { LIGHT_COLOR_TEXT, AVATAR_COLOR, 
-         NAV_BAR_COLOR } from '../../assets/projectJSS/Colors';
+import { createStyles } from "@material-ui/core";
+import {
+  LIGHT_COLOR_TEXT,
+  AVATAR_COLOR,
+  NAV_BAR_COLOR,
+} from "../../assets/projectJSS/Colors";
 
-const styles = () => createStyles({
+const styles = () =>
+  createStyles({
     root: {
-        backgroundColor: NAV_BAR_COLOR
+      backgroundColor: NAV_BAR_COLOR,
+      height: "62px",
     },
-    toolbar: { 
-        display: 'flex',
-        justifyContent: 'space-between',
-        paddingRight: '0px'
+    toolbar: {
+      display: "flex",
+      justifyContent: "space-between",
+      paddingRight: "0px",
     },
     avatar: {
-        backgroundColor: AVATAR_COLOR,
-        margin: '0 5px',
-        textDecoration: 'none'
+      backgroundColor: AVATAR_COLOR,
+      margin: "0 5px",
+      textDecoration: "none",
     },
     userDetails: {
-        display: 'flex',
-        alignItems: 'center'
+      display: "flex",
+      alignItems: "center",
     },
     logoContainer: {
-        display: 'flex',
-        alignItems: 'center',
-        textDecoration: 'none'
+      display: "flex",
+      alignItems: "center",
+      textDecoration: "none",
     },
     logoIcon: {
-        margin: '0 5px',
-        fontSize: '150%'
+      height: "62px",
     },
     logoTitle: {
-        color: LIGHT_COLOR_TEXT
+      color: LIGHT_COLOR_TEXT,
     },
     logoSystemName: {
-        color: 'white',
-        marginLeft: '20px',
-        fontWeight: 'bold'    
+      color: "white",
+      marginLeft: "20px",
+      fontWeight: "bold",
     },
     logoSubTitle: {
-        marginTop: '20px',
-        color: 'white'
-    }
-});
+      marginTop: "20px",
+      color: "white",
+    },
+  });
 
 export default styles;
