4f7a86076199dcfc0004caf1439adf0d42def43d
Shany Guy <gshany@gmail.com>
2020-04-30 15:56:38 +0300

    adding initial base for resume addition

--- src/server/user.ts
+++ src/server/user.ts
@@ -1,42 +1,55 @@
-import { RamadAd, RamadAdSQL  } from "../types/User";
+import { RamadAd, RamadAdSQL } from "../types/User";
 
 export async function fetchFavoriteAds(): Promise<number[]> {
-    const favoriteAds: number[] = await fetch(`/user/favorite`).then(response => {
-        return response.json();
-    });
+  const favoriteAds: number[] = await fetch(`/user/favorite`).then(
+    (response) => {
+      return response.json();
+    }
+  );
 
-    return favoriteAds;
-};
+  return favoriteAds;
+}
 
 export async function unsetFavoriteAd(adId: number): Promise<boolean> {
-    const answer = await fetch(`/user/favorite/${adId}`, {
-        method: 'DELETE'
-    }).then(response => {
-        // Need to think how to determine if the action succeeded
-        return response.status === 200;
-    });
+  const answer = await fetch(`/user/favorite/${adId}`, {
+    method: "DELETE",
+  }).then((response) => {
+    // Need to think how to determine if the action succeeded
+    return response.status === 200;
+  });
 
-    return answer;
-};
+  return answer;
+}
 
 export async function setFavoriteAd(adId: number): Promise<boolean> {
-    const answer = await fetch(`/user/favorite/${adId}`, {
-        method: 'POST'
-    }).then(response => {
-        // Need to think how to determine if the action succeeded
-        return response.status === 200;
-    });
+  const answer = await fetch(`/user/favorite/${adId}`, {
+    method: "POST",
+  }).then((response) => {
+    // Need to think how to determine if the action succeeded
+    return response.status === 200;
+  });
 
-    return answer;
-};
+  return answer;
+}
 
 export async function fetchRamadAds(): Promise<RamadAd[]> {
-    const ramadAdsSQL: RamadAdSQL [] = await fetch('/user/ramad-ads', {
-        method: 'GET'
-    }).then(response => {
-        return response.json();
-    });
-    const allRamadAds: RamadAd[] = ramadAdsSQL.map((ad: RamadAdSQL ) => ad.ramad_ad);
+  const ramadAdsSQL: RamadAdSQL[] = await fetch("/user/ramad-ads", {
+    method: "GET",
+  }).then((response) => {
+    return response.json();
+  });
+  const allRamadAds: RamadAd[] = ramadAdsSQL.map(
+    (ad: RamadAdSQL) => ad.ramad_ad
+  );
 
-    return allRamadAds;
-};
+  return allRamadAds;
+}
+
+export async function saveUserResume(resume: string): Promise<string> {
+  const answer = await fetch(`/user/favorite/${resume}`, {
+    method: "POST",
+  }).then((response) => {
+    return response.json();
+  });
+  return answer;
+}
--- src/store/UserStore.ts
+++ src/store/UserStore.ts
@@ -1,71 +1,84 @@
-import CookieUtils from 'js-cookie';
+import CookieUtils from "js-cookie";
 
-import { observable, decorate, action, computed } from 'mobx';
+import { observable, decorate, action, computed } from "mobx";
 
-import { fetchFavoriteAds, unsetFavoriteAd, setFavoriteAd, fetchRamadAds } from '../server/user';
-import { User, RamadAd } from '../types/User';
+import {
+  fetchFavoriteAds,
+  unsetFavoriteAd,
+  setFavoriteAd,
+  fetchRamadAds,
+  saveUserResume,
+} from "../server/user";
+import { User, RamadAd } from "../types/User";
 
 export class UserStore {
+  private loggedUser: User = JSON.parse(CookieUtils.get("user") || "{}");
+  private favoriteAds = observable.box<number[]>([]);
+  private ramadAds = observable.box<RamadAd[]>([]);
 
+  get getUser() {
+    return this.loggedUser;
+  }
 
-    private loggedUser: User = JSON.parse(CookieUtils.get('user') || '{}');
-    private favoriteAds = observable.box<number[]>([]);
-    private ramadAds = observable.box<RamadAd[]>([]);
+  get getFavoriteAds() {
+    return this.favoriteAds.get();
+  }
 
-    get getUser() {
-        return this.loggedUser;
+  get getRamadAds() {
+    return this.ramadAds.get();
+  }
+
+  get getUserInitials() {
+    const splitName: string[] = this.loggedUser.name.split(" ");
+    return splitName.map((n) => n[0]).join(" ");
+  }
+
+  loadFavoriteAds = async () => {
+    // Fetch data from server
+    const newFavoriteAds: number[] = await fetchFavoriteAds();
+    this.favoriteAds.set(newFavoriteAds);
+  };
+
+  loadRamadAds = async () => {
+    // Fetch data from server
+    const newRamadAds: RamadAd[] = await fetchRamadAds();
+    this.ramadAds.set(newRamadAds);
+  };
+
+  unsetFavoriteAd = async (adId: number) => {
+    try {
+      await unsetFavoriteAd(adId);
+      this.loadFavoriteAds();
+    } catch (error) {
+      console.log("got error, unset function", error);
     }
+  };
 
-    get getFavoriteAds () {
-        return this.favoriteAds.get()
+  setFavoriteAd = async (adId: number) => {
+    try {
+      await setFavoriteAd(adId);
+      this.loadFavoriteAds();
+    } catch (error) {
+      console.log("got error, set function", error);
     }
+  };
 
-    get getRamadAds () {
-        return this.ramadAds.get();
+  saveUserResume = async (resume: string) => {
+    try {
+      await saveUserResume(resume);
+    } catch (error) {
+      console.error(`error while trying to save user: ${resume} resume`, error);
     }
-
-    get getUserInitials () {
-        const splitName: string[] = this.loggedUser.name.split(' ');
-        return splitName.map(n=> n[0]).join(' ');
-    }
-
-    loadFavoriteAds = async () => {
-        // Fetch data from server
-        const newFavoriteAds: number[] = await fetchFavoriteAds();
-        this.favoriteAds.set(newFavoriteAds);
-    }
-
-    loadRamadAds = async () => {
-        // Fetch data from server
-        const newRamadAds: RamadAd[] = await fetchRamadAds();
-        this.ramadAds.set(newRamadAds);
-    }
-
-    unsetFavoriteAd = async (adId: number) => {
-        try {
-            await unsetFavoriteAd(adId);
-            this.loadFavoriteAds();
-        } catch (error) {
-            console.log('got error, unset function', error);
-        }
-    }
-
-    setFavoriteAd = async (adId: number) => {
-        try {
-            await setFavoriteAd(adId);
-            this.loadFavoriteAds();
-        } catch (error) {
-            console.log('got error, set function', error);
-        }
-    }
+  };
 }
 
 decorate(UserStore, {
-    getUser: computed,
-    getUserInitials: computed,
-    getRamadAds: computed,
-    loadRamadAds: action,
-    loadFavoriteAds: action,
-    unsetFavoriteAd: action,
-    setFavoriteAd: action
+  getUser: computed,
+  getUserInitials: computed,
+  getRamadAds: computed,
+  loadRamadAds: action,
+  loadFavoriteAds: action,
+  unsetFavoriteAd: action,
+  setFavoriteAd: action,
+  saveUserResume: action,
 });
